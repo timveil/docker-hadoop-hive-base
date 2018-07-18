a712022131904279f0292db370b81a9b8eba0886
Tim Veil <tjveil@gmail.com>
2018-07-18 17:50:14 -0400

    removing ganglia

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,6 @@
 
 echo "hive base entrypoint"
 
-# Set some sensible defaults
-export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:8020}
-
 function addProperty() {
   local path=$1
   local name=$2
