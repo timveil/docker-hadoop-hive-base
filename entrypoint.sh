e8bf9e87ff4cb7fcb9924175e2025eee9dc059e8
Tim Veil <tjveil@gmail.com>
2018-07-18 17:47:16 -0400

    removing ganglia

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,28 +62,6 @@ if [ "$MULTIHOMED_NETWORK" = "1" ]; then
     addProperty /etc/hadoop/mapred-site.xml yarn.nodemanager.bind-host 0.0.0.0
 fi
 
-if [ -n "$GANGLIA_HOST" ]; then
-    echo "Configuring for ganglia host"
-
-    mv /etc/hadoop/hadoop-metrics.properties /etc/hadoop/hadoop-metrics.properties.orig
-    mv /etc/hadoop/hadoop-metrics2.properties /etc/hadoop/hadoop-metrics2.properties.orig
-
-    for module in mapred jvm rpc ugi; do
-        echo "$module.class=org.apache.hadoop.metrics.ganglia.GangliaContext31"
-        echo "$module.period=10"
-        echo "$module.servers=$GANGLIA_HOST:8649"
-    done > /etc/hadoop/hadoop-metrics.properties
-
-    for module in namenode datanode resourcemanager nodemanager mrappmaster jobhistoryserver; do
-        echo "$module.sink.ganglia.class=org.apache.hadoop.metrics2.sink.ganglia.GangliaSink31"
-        echo "$module.sink.ganglia.period=10"
-        echo "$module.sink.ganglia.supportsparse=true"
-        echo "$module.sink.ganglia.slope=jvm.metrics.gcCount=zero,jvm.metrics.memHeapUsedM=both"
-        echo "$module.sink.ganglia.dmax=jvm.metrics.threadsBlocked=70,jvm.metrics.memHeapUsedM=40"
-        echo "$module.sink.ganglia.servers=$GANGLIA_HOST:8649"
-    done > /etc/hadoop/hadoop-metrics2.properties
-fi
-
 function wait_for_it()
 {
     local serviceport=$1
